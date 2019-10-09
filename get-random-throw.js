67115fc2816d1aca2b6c4e03bbf7d6ba73d138dc
Libby Hamlin <bravolibby@gmail.com>
2019-10-09 10:20:07 -0700

    refactored html

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,15 +1,18 @@
-export const getRandomThrow = () => {
+export const compThrow = playerThrow => {
     const getThrow = Math.floor(Math.random() * 3);
-
-    if (getThrow === 0) {
+    if (playerThrow === 0) {
         return 'rock';
-    } else if (getThrow === 1) {
+    } else if (playerThrow === 1) {
         return 'paper';
-    } else if (getThrow === 2) {
+    } else if (playerThrow === 2) {
         return 'scissors';
     }
 };
 
-console.log(getRandomThrow());
+// console.log(getRandomThrow());
 
 //Your console log test your function
+function compChoice() {
+    const choices = [rock, paper, scissors];
+    console.log(Math.floor(Math.random() * 3);
+    return choices[randomNumber];
